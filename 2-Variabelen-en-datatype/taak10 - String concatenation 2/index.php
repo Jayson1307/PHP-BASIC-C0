23191806d5bc0e342958b5f2a17cdac435e4494d
Jayson1307 <2125083@talnet.nl>
2020-12-07 12:54:26 +0100

    begin opdracht 11

---
+++ 2-Variabelen-en-datatype/taak10 - String concatenation 2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<ul>
+<?php
+    $item = "<li>item1</li> ";
+    $item .= "<li>item2</li> " ;
+    $item .= "<li>item3</li> " ;
+    echo "$item"
+?>   
+</ul>
+</body>
+
+</html>
---
+++ 2-Variabelen-en-datatype/taak11 - var_dump()/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<ul>
+<?php
+
+?>   
+</ul>
+</body>
+
+</html>
