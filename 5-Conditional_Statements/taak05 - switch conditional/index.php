ae136ee67c5381ae87c7b5d4c6150d31b92681e3
Jayson1307 <2125083@talnet.nl>
2020-12-09 20:57:50 +0100

    gedeelte 5 klaar

--- 5-Conditional_Statements/taak05 - switch conditional/index.php
+++ 5-Conditional_Statements/taak05 - switch conditional/index.php
@@ -3,7 +3,97 @@
 <body>
 <ul>
     <?php
-        
+      
+      $score = 10;
+
+      switch($score) {
+          case 10:
+          echo " Wow wat een fantastische score!";
+          break;
+          case 9:
+          echo " Wow bijna alle punten nog 1 te gaan!";
+          break;
+          case 8:
+          echo " Goed bezig, bijna alle punten!";
+          break;
+          case 7:
+          echo " Ga zo door!";
+          break;
+          default:
+          echo " Nog even oefenen dan haal je vast alle punten!";
+      }  
+      $score = 9;
+
+      switch($score) {
+          case 10:
+          echo " Wow wat een fantastische score!";
+          break;
+          case 9:
+          echo " Wow bijna alle punten nog 1 te gaan!";
+          break;
+          case 8:
+          echo " Goed bezig, bijna alle punten!";
+          break;
+          case 7:
+          echo " Ga zo door!";
+          break;
+          default:
+          echo " Nog even oefenen dan haal je vast alle punten!";
+      }  
+      $score = 8;
+
+      switch($score) {
+          case 10:
+          echo " Wow wat een fantastische score!";
+          break;
+          case 9:
+          echo " Wow bijna alle punten nog 1 te gaan!";
+          break;
+          case 8:
+          echo " Goed bezig, bijna alle punten!";
+          break;
+          case 7:
+          echo " Ga zo door!";
+          break;
+          default:
+          echo " Nog even oefenen dan haal je vast alle punten!";
+      }  
+      $score = 7;
+
+      switch($score) {
+          case 10:
+          echo " Wow wat een fantastische score!";
+          break;
+          case 9:
+          echo " Wow bijna alle punten nog 1 te gaan!";
+          break;
+          case 8:
+          echo " Goed bezig, bijna alle punten!";
+          break;
+          case 7:
+          echo " Ga zo door!";
+          break;
+          default:
+          echo " Nog even oefenen dan haal je vast alle punten!";
+      }  
+      $score = 3;
+
+      switch($score) {
+          case 10:
+          echo " Wow wat een fantastische score!";
+          break;
+          case 9:
+          echo " Wow bijna alle punten nog 1 te gaan!";
+          break;
+          case 8:
+          echo " Goed bezig, bijna alle punten!";
+          break;
+          case 7:
+          echo " Ga zo door!";
+          break;
+          default:
+          echo " Nog even oefenen dan haal je vast alle punten!";
+      }  
     ?>
 </ul>     
 </body>
--- 5-Conditional_Statements/taak06 - Logical Operators/index.php
+++ 5-Conditional_Statements/taak06 - Logical Operators/index.php
@@ -3,7 +3,32 @@
 <body>
 <ul>
     <?php
-        
+        $a = 14;
+        $b = 16;
+        if($a < 15 && $b > 15) {
+            echo '<li>$a is kleiner dan 15 en $b is groter dan 15 </li>';
+        }
+        else{
+            echo '<li>$a is groter dan 15 en/of $b is kleiner dan 15 </li>';
+        }
+        if($a > 10 || $b < 20) {
+            echo '<li>$a is groter dan 10 en/of $b is kleiner dan 20 </li>';
+        }
+        else{
+            echo '<li>$a is kleiner dan 10 en $b is groter dan 20 </li>';
+        }
+        if($a > 10 xor  $b < 20) {
+            echo '<li>$a is groter dan 10 en/of $b is kleiner dan 20 </li>';
+        }
+        else{
+            echo '<li>$a is kleiner dan 10 en $b is groter dan 20 </li>';
+        }
+        if($a < 15 != $b > 15) {
+            echo '<li>$a is kleiner dan 15 en $b is groter dan 15 </li>';
+        }
+        else{
+            echo '<li>$a is groter dan 15 en/of $b is kleiner dan 15 </li>';
+        }
     ?>
 </ul>     
 </body>
